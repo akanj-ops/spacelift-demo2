d2a6afb7309297b321e3ebb3c258266b553f9909
akanj-ops <shaheedhaanjum@gmail.com>
2026-07-02 11:22:23 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet from networking stack"
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -32,4 +37,24 @@ resource "aws_subnet" "main" {
 output "subnet_id" {
   value       = aws_subnet.main.id
   description = "ID of the main subnet"
+}
+
+
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+data "aws_vpc" "selected" {
+  id = data.aws_subnet.selected.vpc_id
+}
+
+resource "aws_security_group" "app" {
+  name        = "orbit-labs-app-sg"
+  description = "Security group for Orbit Labs app"
+  vpc_id      = data.aws_vpc.selected.id
+
+  tags = {
+    name    = "Orbit Labs App SG"
+    project = "Orbit-labs"
+  }
 }
